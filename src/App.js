33af673335c911e6d035c757d8a32c8465bf0746
kavyaFullstackDeveloper <kavya.why@gmail.com>
2024-05-24 15:33:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import LoginSignup from './Pages/LoginSignUp';
+import Shop from './Pages/shop';
+import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/assets/banner_mens.png';
+import women_banner from './Components/assets/banner_women.png';
+import kids_banner from './Components/assets/banner_kids.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <BrowserRouter>
+      
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/men' element={<ShopCategory banner={men_banner} category="men"/>}/>
+        <Route path='/women' element={<ShopCategory banner={women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner={kids_banner} category="kid"/>}/>
+        <Route path='/product' element={<Product/>}>
+        <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+
+      </Routes>
+      <Footer/>
+      
+      </BrowserRouter>
+      
     </div>
   );
-}
 
+}
 export default App;
---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="description-navigator">
+        <div className="decription-nav-box">
+            Description 
+        </div>
+        <div className="decription-nav-box fade">
+            Reviews  (122)
+        </div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website is an online platform that facilitates buying and selling
+                of products or services over the internet serves as a virutal market place where business and individual showcase their products, interact with customers, and conduct transactions without the need for a physical presence. 
+                E-coommerce websites have gained immense popularity dure to their convenice accessibility, and the global reach they offer.
+            </p>
+            <p>
+            
+          An e-commerce website is an online platform that enables businesses to sell products and services to consumers over the internet. It typically features a catalog of items, a shopping cart system, and secure payment options, providing a seamless shopping experience from browsing to purchase. E-commerce sites can range from large marketplaces like Amazon to specialized niche stores. They offer convenience, a wide selection, and often competitive pricing, making them a popular choice for modern consumers.
+            </p>
+        </div>
+      </div>
+  )
+}
+
+export default DescriptionBox
